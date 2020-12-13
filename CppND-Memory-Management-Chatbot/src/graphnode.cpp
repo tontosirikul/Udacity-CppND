1271263ecbae060fe31620e7e6a818ee1c63ff69
TonTosirikul <tonydude1999@gmail.com>
2020-12-13 16:15:01 +0700

    add project 3 memory-management-chatbot

---
+++ CppND-Memory-Management-Chatbot/src/graphnode.cpp
@@ -0,0 +1,61 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+    // Task 0 delete _chatBot should be deallocate where it was allocated.
+    //delete _chatBot;
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+// Task 4 applied with change ownership
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
+{
+    _childEdges.push_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+// Task 5 make it can move
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = std::move(chatbot);
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+    //_chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+    // Task 4 must return in smart pointer wat
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
---
+++ CppND-Memory-Management-Chatbot/src/graphnode.h
@@ -0,0 +1,64 @@
+#ifndef GRAPHNODE_H_
+#define GRAPHNODE_H_
+
+#include <vector>
+#include <string>
+#include <memory>
+#include "chatbot.h"
+
+// forward declarations
+class GraphEdge;
+
+class GraphNode
+{
+private:
+    //// STUDENT CODE
+    ////
+
+    // data handles (owned)
+    // Task 4 make it exclusive then apllied to chatlogic.cpp
+    //std::vector<GraphEdge *> _childEdges; // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;
+
+    // data handles (not owned)
+    std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes
+    ChatBot _chatBot;
+
+    ////
+    //// EOF STUDENT CODE
+
+    // proprietary members
+    int _id;
+    std::vector<std::string> _answers;
+
+public:
+    // constructor / destructor
+    GraphNode(int id);
+    ~GraphNode();
+
+    // getter / setter
+    int GetID() { return _id; }
+    int GetNumberOfChildEdges() { return _childEdges.size(); }
+    // Task 4 applied at graphnode.cpp
+    GraphEdge *GetChildEdgeAtIndex(int index);
+    std::vector<std::string> GetAnswers() { return _answers; }
+    int GetNumberOfParents() { return _parentEdges.size(); }
+
+    // proprietary functions
+    void AddToken(std::string token); // add answers to list
+    void AddEdgeToParentNode(GraphEdge *edge);
+    // Task 4 apllied with ownership transfer
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
+
+    //// STUDENT CODE
+    ////
+
+    void MoveChatbotHere(ChatBot chatbot);
+
+    ////
+    //// EOF STUDENT CODE
+
+    void MoveChatbotToNewNode(GraphNode *newNode);
+};
+
+#endif /* GRAPHNODE_H_ */
